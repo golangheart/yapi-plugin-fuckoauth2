33cc1a1e3282fba8670786da86dd51aa10e9d282
phil <zgrubby@gmail.com>
2020-07-23 18:03:53 +0800

    rm controller

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const get = require('lodash.get')
-const controller = require('./controller');
+const find = require('lodash.find')
+const yapi = require('yapi.js');
+const baseController = require('controllers/base.js');
 
 function gets(obj, keys) {
   let value = ''
@@ -21,11 +23,42 @@ function gets(obj, keys) {
   return value
 }
 
+/**
+ * 加载oauth2配置文件
+ */
+function loadOpts() {
+  return find(yapi.WEBCONFIG.plugins, (plugin) => {
+    return plugin.name === 'fuckoauth2';
+  })
+}
+
 module.exports = function (options) {
-  const { authServer, infoPath, userKey, emailKey } = options;
+  const { authServer, infoPath, tokenPath, clientId, clientSecret, userKey, emailKey, redirectUri } = options;
 
   this.bindHook('third_login', async (ctx) => {
-    const token = ctx.request.body.token || ctx.request.query.token;
+
+    const code = ctx.request.body.code || ctx.request.query.code;
+    
+    const tokenResult = await axios.request({
+      method: 'POST',
+      baseURL: authServer,
+      url: tokenPath,
+      params: {
+        grant_type: 'authorization_code',
+        client_id: clientId,
+        client_secret: clientSecret,
+        code: code,
+        redirect_uri: encodeURIComponent(redirectUri)
+      }
+    });
+
+    if (tokenResult.status != 200) {
+      console.error('exchange token err', tokenResult)
+      throw new Error(`${tokenResult.status} ${tokenResult.statusText}`)
+      return
+    }
+
+    const token = tokenResult.data.access_token
 
     const info = await axios.request({
       method: 'GET',
@@ -45,14 +78,5 @@ module.exports = function (options) {
       username: gets(info.data, userKey),
       email: gets(info.data, emailKey)
     };
-  });
-
-  this.bindHook('add_router', function (router) {
-    router({
-      controller: controller,
-      method: 'get',
-      path: 'oauth2/callback',
-      action: 'oauth2Callback'
-    });
   });
 }
